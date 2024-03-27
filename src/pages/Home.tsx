abe44ab5da6323307fa934e64261c56f66613933
Juan Diego <jdgaldames@gmail.com>
2024-03-27 12:17:17 -0300

    Changes on frontend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,18 +9,19 @@ const Home: React.FC = () => {
   return (
     <div style={{ width: '100%', height: '100%', background: '#253237' }}>
       <Parallax ref={parallax} pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#4B4746' }} />
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#4B4746' }} />
 
         <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url('stars', true),
-            backgroundSize: 'cover',
-          }}
-        />
+            offset={0}
+            speed={0}
+            factor={3}
+            style={{
+              backgroundImage: 'url(https://i.imgur.com/ZNV81El.jpg)', // URL directa de la imagen en Imgur
+              backgroundSize: 'cover',
+            }}
+          />
+
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
           
@@ -86,7 +87,7 @@ const Home: React.FC = () => {
             position: 'relative', // Añade posición relativa al contenedor
           }}>
           <div style={{ position: 'absolute', textAlign: 'center', width: '100%', top: '50%', transform: 'translateY(-50%)' }}>
-          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif' }}>Benja agarra la pala</h1>
+          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif', color: '#FFFFFF' }}>Benja agarra la pala</h1>
           <img src="https://i.imgur.com/Ts406fT.png" style={{ width: '30%', margin: 'auto' }} />
           </div>
         </ParallaxLayer>
@@ -103,7 +104,7 @@ const Home: React.FC = () => {
             justifyContent: 'center',
           }}>
           <div style={{ position: 'absolute', textAlign: 'center', width: '100%', top: '50%', transform: 'translateY(-50%)' }}>
-          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif' }}>Impulsamos tu negocio gracias a la IA generativa</h1>
+          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif', color: '#FFFFFF' }}>Impulsamos tu negocio gracias a la IA generativa</h1>
           <img src={url('bash')} style={{ width: '40%', margin: 'auto' }} />
           </div>
         </ParallaxLayer>
@@ -119,7 +120,7 @@ const Home: React.FC = () => {
           }}
           onClick={() => parallax.current.scrollTo(0)}>
           <div style={{ position: 'absolute', textAlign: 'center', width: '100%', top: '50%', transform: 'translateY(-50%)' }}>
-          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif' }}>Acelera tu crecimiento</h1>
+          <h1 className="text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif', color: '#FFFFFF' }}>Acelera tu crecimiento</h1>
           <img src={url('server')} style={{ width: '20%', margin: 'auto' }} />
           </div>
           
